b08a0c8f4d2a3c27e0cee9c0761cbbd268cfa2e7
MRyan <770255975@qq.com>
2021-09-06 10:55:11 +0800

    feat(初始化项目模块)

---
+++ M-test/src/main/java/org/mryan/TestApplication.java
@@ -0,0 +1,12 @@
+package org.mryan;
+
+/**
+ * @description：TODO
+ * @Author MRyan
+ * @Date 2021/9/6 10:01
+ */
+public class TestApplication {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
