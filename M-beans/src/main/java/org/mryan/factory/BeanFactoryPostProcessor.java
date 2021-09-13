dc1bfd0fd20a634d205f7d12905441d09dec7230
MRyan <770255975@qq.com>
2021-09-13 13:19:01 +0800

    feat(准备工作)

---
+++ M-beans/src/main/java/org/mryan/factory/BeanFactoryPostProcessor.java
@@ -0,0 +1,13 @@
+package org.mryan.factory;
+
+/**
+ * @description： 为MPring提供的容器扩展机制
+ * @Author MRyan
+ * @Date 2021/9/13 13:04
+ */
+public interface BeanFactoryPostProcessor {
+
+    //在所有的BeanDefinition加载完成后，实例化Bean对象之前，提供修改BeanDefinition属性机制
+
+
+}
---
+++ M-beans/src/main/java/org/mryan/factory/ConfigurableListableBeanFactory.java
@@ -0,0 +1,11 @@
+package org.mryan.factory;
+
+/**
+ * @description：TODO
+ * @Author MRyan
+ * @Date 2021/9/13 13:13
+ */
+public class ConfigurableListableBeanFactory {
+
+
+}
