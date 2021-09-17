ba428c27b0dd1f1c85040107d6202919d0486135
MRyan <770255975@qq.com>
2021-09-17 21:57:55 +0800

    docs(注释)

--- M-context/src/test/java/org/mryan/ApplicationContextIntegrateTest.java
+++ M-context/src/test/java/org/mryan/ApplicationContextIntegrateTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.mryan.beans.UserService;
 
 /**
- * @description：TODO
+ * @description： 测试应用上下文 自动识别资源加载并扩展
  * @Author MRyan
  * @Date 2021/9/13 22:12
  * @Version 1.0
@@ -14,6 +14,7 @@ public class ApplicationContextIntegrateTest {
     /**
      * 目前Bean的生命周期
      * XML文件-》BeanDefinition-》BeanFactoryPostProcessor修改BeanDefinition-》bean实例化-》BeanPostProcessor前置处理-》执行bean的初始化方法-》BeanPostProcessor后置处理-》使用
+     * getBean: Bean对象-》属性信息-》对象创建-》属性填充-》初始化操作-》注册销毁-》单例判断
      */
     @Test
     public void test_xml() {
