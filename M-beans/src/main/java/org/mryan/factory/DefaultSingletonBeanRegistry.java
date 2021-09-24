83a72798aead48cff93f239c31ae5971dc2eadec
MRyan <770255975@qq.com>
2021-09-24 10:05:44 +0800

    fix(引入NULL_OBJECT导致构建失败)

--- M-beans/src/main/java/org/mryan/factory/DefaultSingletonBeanRegistry.java
+++ M-beans/src/main/java/org/mryan/factory/DefaultSingletonBeanRegistry.java
@@ -22,6 +22,8 @@ public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
 
     private final Map<String, DisposableBean> disposableBeans = new ConcurrentHashMap<>(256);
 
+    protected static final Object NULL_OBJECT = new Object();
+
     @Override
     public Object getSingleton(String beanName) {
         return singletonObjects.get(beanName);
